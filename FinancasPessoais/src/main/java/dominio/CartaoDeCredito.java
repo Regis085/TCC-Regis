bbeb526e3b042942318442951de53e58d2a03248
Regis085 <joseregisneto@gmail.com>
2017-04-30 21:26:39 -0300

    ajustes de layout

--- FinancasPessoais/src/main/java/dominio/CartaoDeCredito.java
+++ FinancasPessoais/src/main/java/dominio/CartaoDeCredito.java
@@ -1,12 +1,16 @@
 package dominio;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "cartao_de_credito")
 public class CartaoDeCredito {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ FinancasPessoais/src/main/webapp/resources/js/menu-flutuante.js
@@ -0,0 +1,13 @@
+﻿jQuery("document").ready(function ($) {
+
+    var container = $('.nav-container');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 89) {
+            container.addClass("flutuar");
+        } else {
+            container.removeClass("flutuar");
+        }
+    });
+
+});
